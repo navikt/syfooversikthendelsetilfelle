1cd837acba4c6e65c7184be8e3d8560a93e46ceb
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-02-13 13:00:06 +0100

    Replace deprecated compile with implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,13 +82,13 @@ dependencies {
     implementation("no.nav.syfo.sm:syfosm-common-models:$smCommonVersion")
     implementation("no.nav.syfo.sm:syfosm-common-kafka:$smCommonVersion")
 
-    compile("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.9.0")
-    compile("org.jetbrains.kotlinx:kotlinx-serialization-runtime-common:0.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-common:0.9.0")
 
-    compile("io.ktor:ktor-jackson:$ktorVersion")
-    compile("io.ktor:ktor-client-jackson:$ktorVersion")
-    compile("io.ktor:ktor-auth:$ktorVersion")
-    compile("io.ktor:ktor-auth-jwt:$ktorVersion")
+    implementation("io.ktor:ktor-jackson:$ktorVersion")
+    implementation("io.ktor:ktor-client-jackson:$ktorVersion")
+    implementation("io.ktor:ktor-auth:$ktorVersion")
+    implementation("io.ktor:ktor-auth-jwt:$ktorVersion")
 
     testImplementation("no.nav:kafka-embedded-env:$kafkaEmbeddedVersion")
     testImplementation("org.amshove.kluent:kluent:$kluentVersion")
