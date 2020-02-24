41d1518763e18bbe6ee9bfbe63f1d720f87a408c
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-02-24 12:41:08 +0100

    Move syketilfelle-func to KSyketilfelle file

--- src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/domain/KSyketilfelledag.kt
+++ src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/domain/KSyketilfelledag.kt
@@ -1,8 +1,40 @@
 package no.nav.syfo.oppfolgingstilfelle.domain
 
 import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 
 data class KSyketilfelledag(
         val dag: LocalDate,
         val prioritertSyketilfellebit: KSyketilfellebit?
 )
+
+const val SYKEPENGESOKNAD = "SYKEPENGESOKNAD"
+const val GRADERT_AKTIVITET = "GRADERT_AKTIVITET"
+const val SYKMELDING = "SYKMELDING"
+
+fun List<KSyketilfelledag>.containsSykmeldingAndSykepengesoknad(): Boolean {
+    return this.containsSykmelding() && this.containsSykepengesoknad()
+}
+
+fun List<KSyketilfelledag>.containsSykmelding(): Boolean {
+    return this
+            .any { it.prioritertSyketilfellebit?.tags?.contains(SYKMELDING) ?: false }
+}
+
+fun List<KSyketilfelledag>.containsSykepengesoknad(): Boolean {
+    return this
+            .any { it.prioritertSyketilfellebit?.tags?.contains(SYKEPENGESOKNAD) ?: false }
+}
+
+fun List<KSyketilfelledag>.isLatestSykmeldingGradert(): Boolean {
+    val sykmeldingerDager = this
+            .filter { it.prioritertSyketilfellebit?.tags?.contains(SYKMELDING) ?: false }
+
+    return if (sykmeldingerDager.isNullOrEmpty()) {
+        false
+    } else {
+        sykmeldingerDager.minBy { ChronoUnit.DAYS.between(it.dag, LocalDate.now()) }!!
+                .prioritertSyketilfellebit!!.tags.contains(GRADERT_AKTIVITET)
+                .or(false)
+    }
+}
