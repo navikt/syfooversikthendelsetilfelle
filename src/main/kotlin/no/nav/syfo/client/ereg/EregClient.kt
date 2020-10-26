83a9fecfa5f4d8b2bc13813ea3454a9ad7b06121
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-10-26 10:09:50 +0100

    Replace Fuel in EregClient with Ktor-client

--- src/main/kotlin/no/nav/syfo/client/ereg/EregClient.kt
+++ src/main/kotlin/no/nav/syfo/client/ereg/EregClient.kt
@@ -1,15 +1,20 @@
 package no.nav.syfo.client.ereg
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.module.kotlin.*
-import com.github.kittinunf.fuel.*
-import com.github.kittinunf.result.*
-import io.ktor.http.HttpHeaders
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.features.json.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
 import no.nav.syfo.client.sts.StsRestClient
-import no.nav.syfo.log
-import no.nav.syfo.metric.COUNT_CALL_EREG_FAIL
 import no.nav.syfo.metric.COUNT_CALL_EREG_SUCCESS
 import no.nav.syfo.util.bearerHeader
+import org.slf4j.LoggerFactory
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class EregOrganisasjonNavn(
@@ -26,28 +31,39 @@ class EregClient(
     private val baseUrl: String,
     private val stsRestClient: StsRestClient
 ) {
+    private val client = HttpClient(CIO) {
+        install(JsonFeature) {
+            serializer = JacksonSerializer {
+                registerKotlinModule()
+                registerModule(JavaTimeModule())
+                configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            }
+        }
+    }
 
     suspend fun hentOrgByOrgnr(orgnr: String, callId: String): EregOrganisasjonResponse? {
         val token = stsRestClient.token()
         val url = "${baseUrl}v1/organisasjon/$orgnr"
-        val (_, response, result) = url
-            .httpGet()
-            .header(mapOf(
-                HttpHeaders.Authorization to bearerHeader(token)
-            ))
-            .responseString()
 
-        return when (result) {
-            is Result.Success -> {
-                COUNT_CALL_EREG_FAIL.inc()
-                jacksonObjectMapper().registerKotlinModule().readValue(result.value)
-            }
-            is Result.Failure -> {
+        val response: HttpResponse = client.get(url) {
+            header(HttpHeaders.Authorization, bearerHeader(token))
+        }
+
+        return when (response.status) {
+            HttpStatusCode.OK -> {
+                val eregOrganisasjonResponse = response.receive<EregOrganisasjonResponse>()
                 COUNT_CALL_EREG_SUCCESS.inc()
-                val exception = result.getException()
-                log.error("Error with responseCode=${response.statusCode} with callId=$callId while requesting Organisasjon from Ereg: ${exception.message}", exception)
+                eregOrganisasjonResponse
+            }
+            else -> {
+                COUNT_CALL_EREG_SUCCESS.inc()
+                LOG.error("Error with responseCode=${response.status.value} with callId=$callId while requesting Organisasjon from Ereg")
                 null
             }
         }
+    }
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(EregClient::class.java)
     }
 }
