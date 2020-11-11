5650d0336703a4cd784a00e2b9b2f40092469017
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-11-11 11:02:27 +0100

    Rename data class Fodselsnummer ot PersonIdentNumber

--- src/main/kotlin/no/nav/syfo/domain/PersonIdentNumber.kt
+++ src/main/kotlin/no/nav/syfo/domain/PersonIdentNumber.kt
@@ -1,11 +1,11 @@
 package no.nav.syfo.domain
 
-data class Fodselsnummer(val value: String) {
+data class PersonIdentNumber(val value: String) {
     private val elevenDigits = Regex("^\\d{11}\$")
 
     init {
         if (!elevenDigits.matches(value)) {
-            throw IllegalArgumentException("$value is not a valid fnr")
+            throw IllegalArgumentException("$value is not a valid PersonIdentNumber")
         }
     }
 }
--- src/test/kotlin/no/nav/syfo/testutil/UserConstants.kt
+++ src/test/kotlin/no/nav/syfo/testutil/UserConstants.kt
@@ -1,7 +1,7 @@
 package no.nav.syfo.testutil
 
 import no.nav.syfo.domain.AktorId
-import no.nav.syfo.domain.Fodselsnummer
+import no.nav.syfo.domain.PersonIdentNumber
 
 object UserConstants {
 
@@ -9,8 +9,8 @@ object UserConstants {
     const val ARBEIDSTAKER_NAME_MIDDLE = "Middle"
     const val ARBEIDSTAKER_NAME_LAST = "Last"
 
-    val ARBEIDSTAKER_FNR = Fodselsnummer("12345678912")
-    val ARBEIDSTAKER_2_FNR = Fodselsnummer(ARBEIDSTAKER_FNR.value.replace("2", "1"))
+    val ARBEIDSTAKER_FNR = PersonIdentNumber("12345678912")
+    val ARBEIDSTAKER_2_FNR = PersonIdentNumber(ARBEIDSTAKER_FNR.value.replace("2", "1"))
     val ARBEIDSTAKER_AKTORID = AktorId(ARBEIDSTAKER_FNR.value + "01")
     val ARBEIDSTAKER_2_AKTORID = AktorId(ARBEIDSTAKER_2_FNR.value + "01")
     const val NAV_ENHET = "0330"
