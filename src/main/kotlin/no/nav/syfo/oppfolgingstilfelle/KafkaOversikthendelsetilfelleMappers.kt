6a3b49078c03b4a22fdeef4eed1f119795bba3be
geir-waagboe <geir.arne.waagbo@nav.no>
2022-03-30 15:36:07 +0200

    Bump dependencies

--- src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/KafkaOversikthendelsetilfelleMappers.kt
+++ src/main/kotlin/no/nav/syfo/oppfolgingstilfelle/KafkaOversikthendelsetilfelleMappers.kt
@@ -4,24 +4,25 @@ import no.nav.syfo.oppfolgingstilfelle.domain.KOversikthendelsetilfelle
 import no.nav.syfo.oppfolgingstilfelle.domain.KSyketilfelledag
 import java.time.LocalDateTime
 
-var mapKOversikthendelsetilfelle =
-    { fnr: String,
-      navn: String,
-      enhetId: String,
-      virksomhetsnummer: String,
-      virksomhetsnavn: String,
-      tidslinje: List<KSyketilfelledag>,
-      tidspunkt: LocalDateTime,
-      gradert: Boolean ->
-        KOversikthendelsetilfelle(
-            fnr = fnr,
-            navn = navn,
-            enhetId = enhetId,
-            virksomhetsnummer = virksomhetsnummer,
-            virksomhetsnavn = virksomhetsnavn,
-            gradert = gradert,
-            fom = tidslinje.first().dag,
-            tom = tidslinje.last().dag,
-            tidspunkt = tidspunkt
-        )
-    }
+var mapKOversikthendelsetilfelle = {
+    fnr: String,
+    navn: String,
+    enhetId: String,
+    virksomhetsnummer: String,
+    virksomhetsnavn: String,
+    tidslinje: List<KSyketilfelledag>,
+    tidspunkt: LocalDateTime,
+    gradert: Boolean
+    ->
+    KOversikthendelsetilfelle(
+        fnr = fnr,
+        navn = navn,
+        enhetId = enhetId,
+        virksomhetsnummer = virksomhetsnummer,
+        virksomhetsnavn = virksomhetsnavn,
+        gradert = gradert,
+        fom = tidslinje.first().dag,
+        tom = tidslinje.last().dag,
+        tidspunkt = tidspunkt
+    )
+}
